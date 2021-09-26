3582063e59a810c155d802e89622b7212a9e7a1c
sans-connaissance <davemalicke@gmail.com>
2021-09-26 16:25:07 -0400

    Initial Commit

---
+++ CS193P-CardGame/CS193P_CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193P_CardGameApp.swift
+//  CS193P-CardGame
+//
+//  Created by David Malicke on 9/26/21.
+//
+
+import SwiftUI
+
+@main
+struct CS193P_CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
