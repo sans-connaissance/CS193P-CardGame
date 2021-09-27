f0c207b9883d0f6e21667c6e48a8a2ba46cb1638
sans-connaissance <davemalicke@gmail.com>
2021-09-26 21:29:18 -0400

    An overview of Swift and SwiftUI

--- CS193P-CardGame/ContentView.swift
+++ CS193P-CardGame/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+            
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+struct CardView: View {
+    
+    var body: some View {
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            
+            Text("✈️")
+                .font(.largeTitle)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
