40697b5ad0378f5f8f80f21991c0fe2d118f8f3a
sans-connaissance <davemalicke@gmail.com>
2021-10-01 13:15:01 -0400

    preparing to branch

--- CS193P-CardGame/Model/MemoryGame.swift
+++ CS193P-CardGame/Model/MemoryGame.swift
@@ -7,17 +7,35 @@
 
 import Foundation
 
-struct MemoryGame <CardContent> {
+struct MemoryGame <CardContent> where CardContent: Equatable {
     private(set)var cards: Array<Card>
     
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
     mutating func choose(_ card: Card) {
-//        if let chosenIndex = cards.firstIndex(where: { aCardInTheCardsArray in aCardInTheCardsArray.id == card.id
+        //        if let chosenIndex = cards.firstIndex(where: { aCardInTheCardsArray in aCardInTheCardsArray.id == card.id
         // simplified version is:
-        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            
             cards[chosenIndex].isFaceUp.toggle()
         }
     }
-
+    
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
         
