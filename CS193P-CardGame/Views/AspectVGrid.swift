e5b759942c585107c9aba2b8e5885a376007363e
sans-connaissance <davemalicke@gmail.com>
2021-10-06 14:01:51 -0400

    finish AspectVGrid

--- CS193P-CardGame/Views/AspectVGrid.swift
+++ CS193P-CardGame/Views/AspectVGrid.swift
@@ -15,14 +15,16 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
     
     var body: some View {
         GeometryReader { geometry in
-            let width: CGFloat = widthThatFits(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio)
-            LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
-                ForEach(items) { item in
-                    content(item).aspectRatio(aspectRatio, contentMode: .fit)
-                    
+            VStack {
+                let width: CGFloat = widthThatFits(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio)
+                LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
+                    ForEach(items) { item in
+                        content(item).aspectRatio(aspectRatio, contentMode: .fit)
+                        
+                    }
                 }
+                Spacer(minLength: 0)
             }
-            
         }
     }
     
